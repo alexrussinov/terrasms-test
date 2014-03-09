58d8a86102999d7e8c9b109a1e1f815909c159a1
Alex <alexrussinov@gmail.com>
2014-03-09 21:44:51 +0400

    initial commit4

--- php/createdatabase.php
+++ php/createdatabase.php
@@ -3,7 +3,7 @@ define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+
 
 
 $db ='mysql:host='.DB_HOST.'.dbname='.DB_NAME.';port='.DB_PORT;

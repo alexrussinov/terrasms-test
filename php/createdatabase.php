5344468b6e09efc891e05dbf59be47a4be93d537
Alex <alexrussinov@gmail.com>
2014-03-09 21:47:59 +0400

    initial commit5

--- php/createdatabase.php
+++ php/createdatabase.php
@@ -1,8 +1,19 @@
 <?php
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+/** The name of the database */
+define('DB_NAME', $_ENV['OPENSHIFT_APP_NAME']);
+
+/** MySQL database username */
+define('DB_USER', $_ENV['OPENSHIFT_MYSQL_DB_USERNAME']);
+
+/** MySQL database password */
+define('DB_PASSWORD', $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']);
+
+/** MySQL hostname */
+define('DB_HOST', $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PORT']);
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
 
 
 

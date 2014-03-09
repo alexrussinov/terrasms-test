c963efba7c0a2df030f542be7397d551d64d30e5
Alex <alexrussinov@gmail.com>
2014-03-09 22:04:47 +0400

    initial commit8

--- php/generatenews.php
+++ php/generatenews.php
@@ -5,9 +5,27 @@
  * @param $pdo
  *
  */
-$db ='mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/;dbname=terrasmstest';
-$user = 'admins7fblhA';
-$pass = 'ExQp6RNYZae1';
+/** The name of the database */
+define('DB_NAME', $_ENV['OPENSHIFT_APP_NAME']);
+
+/** MySQL database username */
+define('DB_USER', $_ENV['OPENSHIFT_MYSQL_DB_USERNAME']);
+
+/** MySQL database password */
+define('DB_PASSWORD', $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']);
+
+/** MySQL hostname */
+define('DB_HOST', $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PORT']);
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+
+
+
+$db ='mysql:host='.DB_HOST.';dbname='.DB_NAME;
+$user = DB_USER;
+$pass = DB_PASSWORD;
 
 // Connection options
 $options = array(
